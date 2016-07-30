eb308523ffa7bb80a3cf323a91fe82eb121766f5
dowdje <dowdje89@gmail.com>
2016-07-30 16:38:05 -0400

    Completed all 4 class creations in has many lab

---
+++ lib/artist.rb
@@ -0,0 +1,40 @@
+class Artist
+  attr_accessor :name, :songs
+
+  @@song_count = 0
+
+
+
+  def initialize(name)
+    @name = name
+    @songs = []
+  end
+
+  def songs
+    @songs
+  end
+
+  def add_song(song)
+    song.artist = self
+    @songs << song
+    @@song_count += 1
+  end
+
+  def add_song_by_name(song)
+    song = Song.new(song)
+    @songs << song
+    @@song_count += 1
+    song.artist = self
+  end
+
+  def self.song_count
+    @@song_count
+  end
+
+
+
+
+
+
+
+end
